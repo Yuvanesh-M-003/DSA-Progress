48663b97f4675ac1b0570f508abd687a4aa38af2
Yuvanesh M <yuvanmuthusamy333@gmail.com>
2026-04-26 08:03:22 +0530

    Time: 1 ms (94.75%), Space: 42.7 MB (81.04%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,24 +1,39 @@
+// class Solution {
+//     public boolean isSubsequence(String s, String t) {
+//     char [] sc = s.toCharArray();
+//     char [] tc = t.toCharArray();
+//     int l=0 , st=0;
+
+//     while(l < sc.length)
+//     {
+//         int r = 0;
+//         for(int i = st;i<tc.length;i++)
+//         {
+//             if(sc[l] == tc[i])
+//             {
+//                 st = i+1;
+//                 r=1;
+//                 break;
+//             }
+//         }
+//         if(r == 0) return false;
+//         l++;
+//     }
+//     return true;
+//     }
+// }
+
 class Solution {
     public boolean isSubsequence(String s, String t) {
-    char [] sc = s.toCharArray();
-    char [] tc = t.toCharArray();
-    int l=0 , st=0;
+        int i = 0, j = 0;
 
-    while(l < sc.length)
-    {
-        int r = 0;
-        for(int i = st;i<tc.length;i++)
-        {
-            if(sc[l] == tc[i])
-            {
-                st = i+1;
-                r=1;
-                break;
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
             }
+            j++;
         }
-        if(r == 0) return false;
-        l++;
-    }
-    return true;
+
+        return i == s.length();
     }
 }
