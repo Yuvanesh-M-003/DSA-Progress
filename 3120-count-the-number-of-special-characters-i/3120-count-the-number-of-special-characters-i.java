79d35bb6339f7ee2ed6ee103196f7d9c176f4ba0
Yuvanesh M <yuvanmuthusamy333@gmail.com>
2026-05-26 13:55:03 +0530

    Time: 1 ms (100%), Space: 43.3 MB (95.05%) - LeetHub

---
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numberOfSpecialChars(String word) {
+        int count = 0;
+
+        for(char ch = 'a'; ch <= 'z'; ch++) {
+            
+            if(word.indexOf(ch) != -1 && 
+               word.indexOf(Character.toUpperCase(ch)) != -1) {
+                
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
