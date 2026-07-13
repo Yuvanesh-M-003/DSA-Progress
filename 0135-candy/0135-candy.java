99d02cba4983528e352f7ded86e6ba2befcbd5ae
Yuvanesh M <yuvanmuthusamy333@gmail.com>
2026-07-13 14:47:02 +0530

    Time: 178 ms (5.05%), Space: 47 MB (98.66%) - LeetHub

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int max = 0;
+        int [] arr = new int [ratings.length];
+        for(int i=0; i<ratings.length; i++)
+        {
+            arr[i] = 1;
+        }
+
+        for(int i=0;i<ratings.length-1;i++)
+        {
+            if(ratings[i] < ratings[i+1])
+            {
+                arr[i+1] = arr[i]+1;
+            }
+        }
+        for(int i=ratings.length-1;i>=1;i--)
+        {
+            if(ratings[i] < ratings[i-1])
+            {
+               arr[i-1] = Math.max(arr[i]+1 , arr[i-1]);
+            }
+        }
+
+        for(int i=0; i<arr.length;i++)
+        {
+            max += arr[i];
+            System.out.print(arr[i]+" ");
+        } 
+        return max;         
+    }
+}
