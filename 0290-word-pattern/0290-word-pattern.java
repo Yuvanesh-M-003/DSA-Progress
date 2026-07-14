310df1316fba615aeb015b429bb3aa0ea726ef6e
Yuvanesh M <yuvanmuthusamy333@gmail.com>
2026-07-14 14:09:37 +0530

    Time: 2 ms (12.42%), Space: 43.1 MB (5.34%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String [] arr = s.split("\\s+");
+        if(pattern.length() != arr.length) return false;
+
+        HashMap<Character , String> map = new HashMap<>();
+        for(int i=0;i<arr.length;i++)
+        {
+            char c = pattern.charAt(i);
+            if(!map.containsKey(c))
+            {
+                if (map.containsValue(arr[i]))
+                {
+                    return false;
+                }
+                map.put(c , arr[i]);
+            }
+            else
+            {
+                if(!map.get(c).equals(arr[i]))
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
