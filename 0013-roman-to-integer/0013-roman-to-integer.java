ac2eaddb9756d7e5989d3e45768c274b95cf5463
Yuvanesh M <yuvanmuthusamy333@gmail.com>
2026-07-17 07:06:34 +0530

    Time: 2 ms (100%), Space: 46.2 MB (93.92%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -8,8 +8,8 @@ class Solution {
                 case 'X': num = 10; break;
                 case 'L': num = 50; break;
                 case 'C': num = 100; break;
-                case 'D': num = 500; break;
-                case 'M': num = 1000; break;
+                case 'D': num = 500; break;   
+                case 'M': num = 1000; break; 
             }
             if (4 * num < ans) ans -= num;
             else ans += num;
