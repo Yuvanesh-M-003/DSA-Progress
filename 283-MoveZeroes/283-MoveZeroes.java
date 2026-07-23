76e1e130222c4cf707ee0fe3eab738520af8f524
Yuvanesh M <yuvanmuthusamy333@gmail.com>
2026-07-23 16:32:27 +0530

    File updated at 7/23/2026, 4:32:26 PM

--- 283-MoveZeroes/283-MoveZeroes.java
+++ 283-MoveZeroes/283-MoveZeroes.java
@@ -1,20 +1,16 @@
-// Last updated: 7/23/2026, 4:10:33 PM
+// Last updated: 7/23/2026, 4:32:26 PM
 1class Solution {
 2    public void moveZeroes(int[] nums) {
-3        int count =0,n=0;
-4        for(int i=0;i<nums.length;i++)
+3        int c = 0 , n= nums.length;
+4        for(int i=0;i<n;i++)
 5        {
-6            if(nums[i]!=0)
-7            {
-8                nums[count]=nums[i];
-9                count++;
-10                n++;
-11            }
-12        }
-13        for(int i=0;i<nums.length-n;i++)
-14        {
-15            nums[count]=0;
-16            count++;
-17        }
-18    }
-19}
+6           if(nums[i] != 0)
+7           {
+8            int temp = nums[i];
+9            nums[i] = nums[c];
+10            nums[c] = temp;
+11            c++;
+12           }
+13        }
+14    }
+15}
