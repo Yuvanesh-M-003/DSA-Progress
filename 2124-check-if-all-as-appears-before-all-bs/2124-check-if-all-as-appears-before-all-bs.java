d2c02b116b52a566c10f43b5df7c5a7b1a09dd7a
Yuvanesh M <yuvanmuthusamy333@gmail.com>
2026-07-24 17:36:28 +0530

    Time: 0 ms (100%), Space: 42.8 MB (63.34%) - LeetHub

--- 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.java
+++ 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.java
@@ -1,6 +1,6 @@
 class Solution {
         public boolean checkString(String s) {
-        for (int i = 1; i < s.length(); ++i) {
+        for (int i = 1; i < s.length();i++) {
             if (s.charAt(i - 1) == 'b' && s.charAt(i) == 'a') {
                 return false;
             }
