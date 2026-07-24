034692310a3d3ebd7172fb330876336ca6806583
Yuvanesh M <yuvanmuthusamy333@gmail.com>
2026-07-24 17:35:27 +0530

    Create 2124-CheckifAllA'sAppearsBeforeAllB's

---
+++ 2124-CheckifAllA'sAppearsBeforeAllB's/2124-CheckifAllA'sAppearsBeforeAllB's.java
@@ -0,0 +1,11 @@
+// Last updated: 7/24/2026, 5:35:27 PM
+1class Solution {
+2        public boolean checkString(String s) {
+3        for (int i = 1; i < s.length(); ++i) {
+4            if (s.charAt(i - 1) == 'b' && s.charAt(i) == 'a') {
+5                return false;
+6            }
+7        }
+8        return true;
+9    }
+10}
