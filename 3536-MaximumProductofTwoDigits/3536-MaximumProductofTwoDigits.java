5a03482c1b26b650af2c4910c655f0e42f08a42d
Yuvanesh M <yuvanmuthusamy333@gmail.com>
2026-07-25 10:22:41 +0530

    File updated at 7/25/2026, 10:22:41 AM

--- 3536-MaximumProductofTwoDigits/3536-MaximumProductofTwoDigits.java
+++ 3536-MaximumProductofTwoDigits/3536-MaximumProductofTwoDigits.java
@@ -1,17 +1,27 @@
-// Last updated: 7/25/2026, 10:17:13 AM
+// Last updated: 7/25/2026, 10:22:41 AM
 1class Solution {
 2    public int maxProduct(int n) {
 3        int prod = 1;
 4        int max1 = 0, max2 = 0 ;
-5        ArrayList<Integer> arr = new ArrayList<>();
+5        //ArrayList<Integer> arr = new ArrayList<>();
 6        while(n != 0)
 7        {
 8            int dig = n%10;
-9            arr.add(dig);
-10            n /= 10;
-11        }
-12        Collections.sort(arr);
-13        prod = arr.get(arr.size()-1) * arr.get(arr.size()-2);
-14        return prod;
-15    }
-16}
+9            if(dig > max1)
+10            {
+11                max2 = max1;
+12                max1 = dig;
+13            }
+14            else if(dig <= max1 && dig > max2)
+15            {
+16                max2 = dig;
+17            }
+18            //arr.add(dig);
+19            n /= 10;
+20        }
+21        // Collections.sort(arr);
+22        // prod = arr.get(arr.size()-1) * arr.get(arr.size()-2);
+23        prod = max1 * max2;
+24        return prod;
+25    }
+26}
