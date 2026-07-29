29b073129f27e4a1d609b6737eb90d9cd3bbe7cc
Yuvanesh M <yuvanmuthusamy333@gmail.com>
2026-07-29 17:58:51 +0530

    Create 3995-GcdOfOddAndEvenSums

---
+++ 3995-GcdOfOddAndEvenSums/3995-GcdOfOddAndEvenSums.java
@@ -0,0 +1,26 @@
+// Last updated: 7/29/2026, 5:58:51 PM
+class Solution {
+    public int gcdOfOddEvenSums(int n) {
+        int eve = 0, odd = 0 ,ans = 0;
+        for(int i=1; i<=(n*2); i++)
+        {
+            if(i %2 == 0)
+            {
+                eve += i;
+            }
+            else
+            {
+                odd += i;
+            }
+        }
+        ans = gcd(odd , eve);
+        return ans;
+    }
+
+    public static int gcd(int odd , int eve)
+    {
+        if(eve==0) return odd;
+
+        return gcd(eve , odd % eve);
+    }
+}
