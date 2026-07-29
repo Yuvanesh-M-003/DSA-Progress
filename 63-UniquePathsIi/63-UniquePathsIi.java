3eca11478ea47a6d8c29adc8cc8a3be6dab993b4
Yuvanesh M <yuvanmuthusamy333@gmail.com>
2026-07-29 18:04:41 +0530

    Create 63-UniquePathsIi

---
+++ 63-UniquePathsIi/63-UniquePathsIi.java
@@ -0,0 +1,28 @@
+// Last updated: 7/29/2026, 6:04:41 PM
+class Solution {
+    public int uniquePathsWithObstacles(int[][] ob) {
+          int m=ob.length;
+          int n=ob[0].length;
+          int [][] dp = new int[m][n];
+          if(ob[0][0]==1) return 0;
+          dp[0][0] =1;
+          for(int i=1;i<m;i++)
+          {
+            if(ob[i][0] ==0) dp [i][0] =dp[i-1][0];
+          }
+          for(int i=1;i<n;i++)
+          {
+            if(ob[0][i] ==0) dp [0][i] =dp[0][i-1];
+          }
+          for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(ob[i][j] == 0)
+                {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                }
+                else dp[i][j] = 0;
+            }
+          }
+          return dp[m-1][n-1];
+    }
+}
