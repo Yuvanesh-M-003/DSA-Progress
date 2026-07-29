a4bed2304410d6cc00ad40cc9c8036f05e59484f
Yuvanesh M <yuvanmuthusamy333@gmail.com>
2026-07-29 17:59:43 +0530

    Create 1987-SubstringsOfSizeThreeWithDistinctCharacters

---
+++ 1987-SubstringsOfSizeThreeWithDistinctCharacters/1987-SubstringsOfSizeThreeWithDistinctCharacters.java
@@ -0,0 +1,16 @@
+// Last updated: 7/29/2026, 5:59:42 PM
+class Solution 
+{
+    public int countGoodSubstrings(String s) 
+    {
+      int count=0;
+      for(int i=0;i<s.length()-2;i++)
+      {
+        if(s.charAt(i)!=s.charAt(i+1)&&s.charAt(i+1)!=s.charAt(i+2)&&s.charAt(i)!=s.charAt(i+2))
+        {
+            count++;
+        }
+      }
+      return count++;
+    }
+}
