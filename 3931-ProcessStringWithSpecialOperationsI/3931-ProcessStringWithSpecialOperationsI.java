1c7e39c766f965a90642e15e91a0d9cbb4177410
Yuvanesh M <yuvanmuthusamy333@gmail.com>
2026-07-29 17:58:56 +0530

    Create 3931-ProcessStringWithSpecialOperationsI

---
+++ 3931-ProcessStringWithSpecialOperationsI/3931-ProcessStringWithSpecialOperationsI.java
@@ -0,0 +1,27 @@
+// Last updated: 7/29/2026, 5:58:55 PM
+class Solution {
+    public String processStr(String s) {
+        StringBuilder res = new StringBuilder();
+        for(int i=0; i<s.length(); i++)
+        {
+            char c = s.charAt(i);
+            if(c >= 'a' && c <= 'z')
+            {
+                res.append(c);
+            }
+            if(c == '*' && !res.isEmpty())
+            {
+                res.setLength(res.length() - 1);
+            }
+            if(c == '#' && !res.isEmpty())
+            {
+                res = res.append(res);
+            }
+            if(c == '%' && !res.isEmpty())
+            {
+                res = res.reverse();
+            }
+        }
+        return res.toString();
+    }
+}
