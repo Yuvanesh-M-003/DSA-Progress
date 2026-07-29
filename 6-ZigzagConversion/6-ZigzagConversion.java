4873e8e8b77d0e0e6dbb6a3a2719b5ce571bded1
Yuvanesh M <yuvanmuthusamy333@gmail.com>
2026-07-29 18:05:49 +0530

    Create 6-ZigzagConversion

---
+++ 6-ZigzagConversion/6-ZigzagConversion.java
@@ -0,0 +1,28 @@
+// Last updated: 7/29/2026, 6:05:48 PM
+class Solution {
+    public String convert(String s, int n) {
+        if (n == 1 || n > s.length()) {
+            return s;
+        }
+        List<StringBuilder> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(new StringBuilder());
+        }
+        int index = 0;
+        int direction = 0;
+        for (char c : s.toCharArray()) {
+            list.get(index).append(c);
+            if (index == 0) {
+                direction = 1;
+            } else if (index == n - 1) {
+                direction = -1;
+            }
+            index = index + direction;
+        }
+        StringBuilder result = new StringBuilder();
+        for (StringBuilder x : list) {
+            result.append(x);
+        }
+        return result.toString();
+    }
+}
