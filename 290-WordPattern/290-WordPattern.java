fb39b25efa9123a2893a7395fa663d939f6e1dbf
Yuvanesh M <yuvanmuthusamy333@gmail.com>
2026-07-29 18:02:01 +0530

    Create 290-WordPattern

---
+++ 290-WordPattern/290-WordPattern.java
@@ -0,0 +1,29 @@
+// Last updated: 7/29/2026, 6:02:00 PM
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String [] arr = s.split("\\s+");
+        if(pattern.length() != arr.length) return false;
+
+        HashMap<Character , String> map = new HashMap<>();
+        for(int i=0;i<arr.length;i++)
+        {
+            char c = pattern.charAt(i);
+            if(!map.containsKey(c))
+            {
+                if (map.containsValue(arr[i]))
+                {
+                    return false;
+                }
+                map.put(c , arr[i]);
+            }
+            else
+            {
+                if(!map.get(c).equals(arr[i]))
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
