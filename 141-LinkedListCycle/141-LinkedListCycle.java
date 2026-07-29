99e2618f5807ec70fdd713a4a95d153beb174de0
Yuvanesh M <yuvanmuthusamy333@gmail.com>
2026-07-29 18:03:20 +0530

    Create 141-LinkedListCycle

---
+++ 141-LinkedListCycle/141-LinkedListCycle.java
@@ -0,0 +1,28 @@
+// Last updated: 7/29/2026, 6:03:20 PM
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        if(head == null || head.next == null) return false;
+        fast = head.next;
+        while(fast != null && fast.next!=null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow ==  fast ) return true;
+            
+        }
+        return false;
+    }
+}
