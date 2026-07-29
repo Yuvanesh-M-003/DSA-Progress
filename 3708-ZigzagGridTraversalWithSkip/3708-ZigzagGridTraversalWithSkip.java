cbd89001a86b9ecc31fdfbddffe22bc0cf83da43
Yuvanesh M <yuvanmuthusamy333@gmail.com>
2026-07-29 17:59:02 +0530

    Create 3708-ZigzagGridTraversalWithSkip

---
+++ 3708-ZigzagGridTraversalWithSkip/3708-ZigzagGridTraversalWithSkip.java
@@ -0,0 +1,31 @@
+// Last updated: 7/29/2026, 5:59:01 PM
+class Solution {
+    public List<Integer> zigzagTraversal(int[][] grid) {
+        ArrayList <Integer> ans = new ArrayList<>();
+        int n = grid.length , m = grid[0].length;
+        for(int i=0;i<n;i++)
+        {
+            if(i%2 == 0 || i == 0)
+            {
+                for(int j=0;j<m;j++)
+                {
+                    if(j == 0 || j%2 == 0)
+                    {
+                        ans.add(grid[i][j]);
+                    }
+                }
+            }
+            else
+            {
+                for(int j=m-1;j>=0;j--)
+                {
+                    if(j%2 != 0)
+                    {
+                        ans.add(grid[i][j]);
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+}
