71c0fa8c5eee3ddaa122da346c9e4b8d941318f4
Yuvanesh M <yuvanmuthusamy333@gmail.com>
2026-07-29 17:58:53 +0530

    Create 3973-FlipSquareSubmatrixVertically

---
+++ 3973-FlipSquareSubmatrixVertically/3973-FlipSquareSubmatrixVertically.java
@@ -0,0 +1,13 @@
+// Last updated: 7/29/2026, 5:58:52 PM
+class Solution {
+    public int[][] reverseSubmatrix(int[][] grid, int x, int y, int k) {
+        for(int i = 0; i < k / 2; i++){
+            for(int j = 0; j < k; j++){
+                int temp = grid[x + i][y + j];
+                grid[x + i][y + j] = grid[x + k - 1 - i][y + j];
+                grid[x + k - 1 - i][y + j] = temp;
+            }
+        }
+        return grid;
+    }
+}
