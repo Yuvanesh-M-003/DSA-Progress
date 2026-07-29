720ac6525c5ca9926a579025f38862f44ffae984
Yuvanesh M <yuvanmuthusamy333@gmail.com>
2026-07-29 18:02:41 +0530

    Create 205-IsomorphicStrings

---
+++ 205-IsomorphicStrings/205-IsomorphicStrings.java
@@ -0,0 +1,29 @@
+// Last updated: 7/29/2026, 6:02:40 PM
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        if(s.length() != t.length()) return false;
+
+        HashMap<Character , Character> map = new HashMap<>();
+        for(int i=0;i<s.length();i++)
+        {
+            char c1 = s.charAt(i);
+            char c2 = t.charAt(i);
+            if(!map.containsKey(c1))
+            {
+                if(map.containsValue(c2))
+                {
+                    return false;
+                }
+                map.put(c1 , c2);
+            }
+            else
+            {
+                if(map.get(c1) != c2)
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
