e5fc678cad6e31e745a1952f3b4dbb428af3bd3e
Yuvanesh M <yuvanmuthusamy333@gmail.com>
2026-07-29 17:59:16 +0530

    Create 3227-FindMissingAndRepeatedValues

---
+++ 3227-FindMissingAndRepeatedValues/3227-FindMissingAndRepeatedValues.java
@@ -0,0 +1,25 @@
+// Last updated: 7/29/2026, 5:59:15 PM
+class Solution {
+    public int[] findMissingAndRepeatedValues(int[][] grid) {
+    int n = (grid.length) * (grid[0].length);
+    int ex = (n * (n + 1)) / 2;
+    int tot = 0 , num = 0;
+    int [] ans = new int [2];
+    HashMap <Integer , Integer> map = new HashMap<>();
+    for(int i = 0;i<grid.length; i++)
+    {
+        for(int j =0 ;j < grid[0].length;j++)
+        {
+            tot += grid[i][j];
+            map.put(grid[i][j] , map.getOrDefault(grid[i][j] , 0)+1);
+            if(map.get(grid[i][j]) == 2)
+            {
+                num = grid[i][j];
+            }
+        }
+    }
+    ans[0] = num;
+    ans[1] = ex - (tot - num);  
+    return ans;
+    }
+}
